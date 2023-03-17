1fc49286bd0c5299401444f404a8442aedb00e26
Tawakalt10 <raheemtawa@email.com>
2023-03-17 14:34:47 +0800

    new changes

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-console.log("Tao");
+// Show navigation menu
+const navMenu = document.getElementById("nav-menu");
+const navToggle = document.getElementById("nav-toggle");
+const navClose = document.getElementById("nav-close");
+
+if (navToggle) {
+  navToggle.addEventListener("click", function () {
+    navMenu.classList.add("show");
+  });
+}
+
+// Hide Navigation Menu
+if (navClose) {
+  navClose.addEventListener("click", function () {
+    navMenu.classList.remove("show");
+  });
+}
+
+// remvove menu when cicked
+const navLink = document.querySelectorAll(".nav__link");
+const linkAction = () => {
+  const navMenu = document.getElementById("nav-menu");
+  navMenu.classList.remove("show");
+};
+
+navLink.forEach((link) => {
+  link.addEventListener("click", linkAction);
+});
+
+// END OF NAVIGATION TOGGLE
